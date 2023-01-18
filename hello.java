1270c3d607e61f6404da7d4ee20c61508850ed11
SANIKOMMU SAI NITIN REDDY <87408675+Nitinreddy17@users.noreply.github.com>
2023-01-18 08:11:54 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,16 +1,11 @@
 import java.util.Scanner; // Import the Scanner class
 
-class MyClass {
-  public static void main(String[] args) {
-    int x, y, sum;
-    Scanner myObj = new Scanner(System.in); // Create a Scanner object
-    System.out.println("Type a number:");
-    x = myObj.nextInt(); // Read user input
-
-    System.out.println("Type another number:");
-    y = myObj.nextInt(); // Read user input
-
-    sum = x + y;  // Calculate the sum of x + y
-    System.out.println("Sum is: " + sum); // Print the sum
-  }
-} 
+public class HelloWorld
+{
+	// Your program begins with a call to main().
+	// Prints "Hello, World" to the terminal window.
+	public static void main(String args[])
+	{
+		System.out.println("Hello, World");
+	}
+}
